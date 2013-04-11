0f42e05002e685a96ae2745e7edd8e373d6aabf5
John Tarling <johntarling@gmail.com>
2013-04-11 22:35:53 +0100

    Add Home route, refactor article route and deal with NotFoundHttpExceptions nicely

--- src/app.php
+++ src/app.php
@@ -18,8 +18,32 @@ $app->get('/setup', function() use ($app) {
 	return "done";
 });
 
+// Home route
+$app->get('/', function() use ($app) {
+	//Get latest 10 news articles
+	$articles = $app['news_service']->getLatestNewsArticles(10);
+	return $app['twig']->render('home.html', array(
+        'articles' => $articles,
+    ));
+});
+
+// View article route
 $app->get('/{id}', function ($id) use ($app) {
-	$sql = "SELECT * FROM news WHERE id = ?";
-	$post = $app['db']->fetchAssoc($sql, array($id));
-    return 'Hello '.$app->escape($post["title"]);
+	//Get article
+    $article = $app['news_service']->getArticleById($id);
+    if ($article != null) {
+    	return $app['twig']->render('article.html', array(
+        	'article' => $article,
+      	));
+    } else {
+    	throw new Symfony\Component\HttpKernel\Exception\NotFoundHttpException("No articles were found for the id specified.");
+    }
+});
+
+// Handle Not found errors
+$app->error(function (Symfony\Component\HttpKernel\Exception\NotFoundHttpException $e) use ($app) {
+    $message = $e->getMessage() ?: 'Resource not found!';
+    return new Symfony\Component\HttpFoundation\Response(
+    	$app['twig']->render('error.html', array('message' => $message)
+    ), 404);
 });
